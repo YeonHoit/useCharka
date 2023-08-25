29d6422930ba500ab5cdff99cdf4ca00de4b8228
YeonHoit <pdbx12@gmail.com>
2023-08-25 11:15:05 +0900

    Select

--- src/app/(sub)/select/SelectComponent.tsx
+++ src/app/(sub)/select/SelectComponent.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { Box, Select } from "@chakra-ui/react";
+import { MdArrowDropDown } from "react-icons/md";
 
 export default function SelectComponent() {
   return (
     <Box p={6}>
       <Select
-        placeholder="Select Option"
         onChange={(e) => {
           const value = e.currentTarget.value;
           console.log(value);
         }}
         size={"lg"}
-        variant={"filled"}
+        variant={"outline"}
+        icon={<MdArrowDropDown />}
+        bg={"tomato"}
+        color={"white"}
       >
         <option
           value="option1"
@@ -20,12 +23,33 @@ export default function SelectComponent() {
             fontSize: "16px",
             fontWeight: "bold",
             border: "1px solid black",
+            color: "black",
           }}
         >
           Option 1
         </option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
+        <option
+          value="option2"
+          style={{
+            fontSize: "16px",
+            fontWeight: "bold",
+            border: "1px solid black",
+            color: "black",
+          }}
+        >
+          Option 2
+        </option>
+        <option
+          value="option3"
+          style={{
+            fontSize: "16px",
+            fontWeight: "bold",
+            border: "1px solid black",
+            color: "black",
+          }}
+        >
+          Option 3
+        </option>
       </Select>
     </Box>
   );
